c8a449aa868dc015e8df8a80d79e8bd8428dc07d
elbekdavron <elbekd4vronov@gmail.com>
2025-01-25 20:26:48 +0500

    random name sender, created new bd.json

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-from tinydb import TinyDB
+from tinydb import TinyDB, Query
+import requests
 
-db = TinyDB('db.json') 
+db = TinyDB('bd.json') 
 
-db.insert(
-    {
-        'name': 'Behzod', 
-        'age': 35, 
-        'job': 'economist'
-    }
-)
-db.insert(
-    {
-        'name': 'Akobir', 
-        'age': 19, 
-        'job': 'teacher'
-    }
-)
+headers = {
+    'X-Api-Key':"4a322f608fd24b69ae2e0c50ecbfb2a3"
+}
 
+response = requests.get('https://randommer.io/api/Name?nameType=fullname&quantity=10', headers=headers).json()
+
+
+for user in response:
+    db.insert({'name':f"{user}"})
+
+db.remove(doc_ids=range(1, 23))
