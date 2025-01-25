1f9b8f3b5c38c9a4ace6824e0690de58883654d1
elbekdavron <elbekd4vronov@gmail.com>
2025-01-25 19:43:49 +0500

    insert db2

--- main.py
+++ main.py
@@ -2,4 +2,18 @@ from tinydb import TinyDB
 
 db = TinyDB('db.json') 
 
-db.insert({'name': 'Zarif', 'age': 35})
+db.insert(
+    {
+        'name': 'Behzod', 
+        'age': 35, 
+        'job': 'economist'
+    }
+)
+db.insert(
+    {
+        'name': 'Akobir', 
+        'age': 19, 
+        'job': 'teacher'
+    }
+)
+
